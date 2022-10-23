90250dc4dbf88c0f6e79dcd182bf5eb9c58d6431
Tuna Onal <79300320+onalltuna@users.noreply.github.com>
2022-10-23 15:27:24 +0300

    Add files via upload
    Comments added for Problem1 Part1 and Part2

--- 304hw1_Problem1_Part1.c
+++ 304hw1_Problem1_Part1.c
@@ -5,17 +5,21 @@
 #include <sys/time.h>
 #include <signal.h>
 
-
-int main() 
+int main()
 {
+    // pid_t data type to hold pid values
     pid_t pid;
     struct timeval current_time;
+    // fork for creating the child
     pid = fork();
 
+    // if pid is 0 then this is the child process
     if (pid == 0)
     {
         for (int i = 0; i < 5; i++)
         {
+            // child prints its own pid and current time then sleeps 1 seconds
+            // in this way child prints current time and its pid every second
             printf("\nPID of child = %d\n", getpid());
             struct timeval current_time;
             gettimeofday(&current_time, NULL);
@@ -23,12 +27,14 @@ int main()
             sleep(1);
         }
     }
+    // if pid is greater than 0 then this is the parent process
     else if (pid > 0)
     {
+        // parent process sleeps for 5 seconds then kills child process,prints its output and then terminates
         sleep(5);
         kill(pid, SIGTERM);
         printf("Child <%d> killed \n", pid);
-        kill(getpid(),SIGTERM);
+        kill(getpid(), SIGTERM);
     }
 
     return 0;
--- 304hw1_Problem1_Part2.c
+++ 304hw1_Problem1_Part2.c
@@ -7,12 +7,20 @@
 
 int main()
 {
+    // list of pid_t to store the pids of child processes
     pid_t pids[4];
+
+    // for loop executes 4 time to create 4 child processes
     for (int i = 0; i < 4; i++)
     {
+        // store the ith childs pid in pids list
         pids[i] = fork();
+
+        // if pid equals zero then this is one of the child processes
         if (pids[i] == 0)
         {
+            // similar to the part1 each child prints its own pid and current time
+            // once a second for 5 seconds
             for (int k = 0; k < 5; k++)
             {
                 struct timeval current_time;
@@ -20,16 +28,18 @@ int main()
                 printf("PID of child = %d  seconds : %ld\n", getpid(), current_time.tv_sec);
                 sleep(1);
             }
+            // kill extra grandchildren othervise there would be 2^4 processes
             kill(getpid(), SIGTERM);
         }
     }
+    // parent process waits for 5 seconds and kills all the child processes and prints kill message
     sleep(5);
     for (int i = 0; i < 4; i++)
     {
         kill(pids[i], SIGTERM);
         printf("Child <%d> is killed\n", pids[i]);
     }
-
+    // finally parent process terminates
     kill(getpid(), SIGTERM);
 
     return 0;
