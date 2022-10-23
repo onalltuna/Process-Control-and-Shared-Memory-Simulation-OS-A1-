3511e5ddfafa4083ab1c8f4a4dac88e3633adccc
Tuna Onal <79300320+onalltuna@users.noreply.github.com>
2022-10-23 17:18:20 +0300

    Add files via upload
    Comments added for the Promlem3 (driver.c and consumer-producer.c)

--- consumer-producer.c
+++ consumer-producer.c
@@ -12,75 +12,90 @@
 
 int main(int argc, char *argv[]) {
 
-    // printf("%d\n",argc);
-    // printf("%s\n",argv[0]);
-    // printf("%s\n",argv[1]);
-    // printf("%s\n",argv[2]);
-    // printf("%s\n",argv[3]);
-
-    const int SIZE = 4096;
-    int shm_fd;
+    //name and the size of the shared memory
     const char *name = "SHM";
+    const int SIZE = 4096;
+    
+    int shm_fd;
     char *ptr;
     char *ptr2;
 
+    //ran1 and ran2 are the random values for swaping the characters of the input string
+    //seed with the pid for each child process
     int ran1,ran2;
     srand (getpid());
 
-
+    //if input from driver program is 0 this is the first child
     if (strcmp(argv[1],"0") == 0) {
 
+        //print the starting string of the game
         printf("Child %d : %s\n",atoi(argv[1]) + 1, argv[3]);
-
+        //open a shared memory read/write
+        //adjust the size of the shared memory
         shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
         ftruncate(shm_fd, SIZE);
 
+        //map the character pointer to the shared memory
         ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        //check if mapping is succesful
         if (ptr == MAP_FAILED)
         {
             printf("Map failed\n");
             return -1;
         }
 
+        //change the value of the string in the shared memory to starting string
         sprintf(ptr, "%s", argv[3]);
         ptr += strlen(argv[3]);
     }
+    //case for the children which are not first or last
     else if (atoi(argv[1]) > 0 )
     {
-        // printf("argv1 : %d---argv2 : %d\n",atoi(argv[1]),atoi(argv[2]));
+        //open the shared memory
         shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
         ftruncate(shm_fd, SIZE);
 
+        //map to the shared memory
         ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        //check if mapping is succesful
         if (ptr == MAP_FAILED)
         {
             printf("Map failed\n");
             return -1;
         }
 
+        //ptr2 is another character pointer to be used to swapping characters of ptr
         ptr2 = ptr;
+        //choose random values for swapping
         ran1 = rand() % strlen(ptr2) + 1;
         ran2 = rand() % strlen(ptr2) + 1;
+        //swap the selected characters
         char x = ptr2[ran2];
         ptr2[ran2] = ptr2[ran1];
         ptr2[ran1] = x;
 
+        //store the new string in the shared memory
         strcpy(ptr, ptr2);
-
+        //print the swapped string
         printf("Child %d : %s\n", atoi(argv[1]) + 1, ptr);
     }
+    //case for the last child
     else if ((atoi(argv[1]) < 0))
     {
+        //open the shared memory
         shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
         ftruncate(shm_fd, SIZE);
 
+        //map to the shared memory
         ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        //check if the mapping is succesful
         if (ptr == MAP_FAILED)
         {
             printf("Map failed\n");
             return -1;
         }
 
+        //swap the characters of the string in the shared memory
         ptr2 = ptr;
         ran1 = rand() % strlen(ptr2) + 1;
         ran2 = rand() % strlen(ptr2) + 1;
@@ -90,8 +105,10 @@ int main(int argc, char *argv[]) {
 
         strcpy(ptr, ptr2);
 
+        //print the swapped string
         printf("Child %d : %s\n", -1 * atoi(argv[1]) + 1, ptr);
 
+        //unlink the shared memory because this is the last child
         if (shm_unlink(name) == -1) {
 		printf("Error removing %s\n",name);
 		exit(-1);
--- driver.c
+++ driver.c
@@ -9,60 +9,77 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-char *newargv[] = { NULL, argv[2], argv[2], argv[3], NULL };
-char *newenviron[] = { NULL };
+  // newargv will the arguments for the consumer-producer program
+  char *newargv[] = {NULL, argv[2], argv[2], argv[3], NULL};
+  char *newenviron[] = {NULL};
 
-const char *name = "SHM";
-const int SIZE = 4096;
+  // name of the shared memory will be SHM and size will be 4096
+  const char *name = "SHM";
+  const int SIZE = 4096;
 
-int shm_fd;
-char *ptr;
+  int shm_fd;
+  char *ptr;
 
-if (argc <= 2 || atoi(argv[2]) < 2) {
+  // check if the number of command line argments at least 3 and number of childs is at least 2
+  if (argc <= 2 || atoi(argv[2]) < 2)
+  {
     fprintf(stderr, "Usage: %s <file-to-exec>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
+  // number of childs(number of players of the chinese whisper game) is the 3rd command line argumant
+  // and the name of the consumer-producer program is the second command line argument
   int childNum = atoi(argv[2]);
-  char* consumerName = argv[1];
+  char *consumerName = argv[1];
 
   newargv[0] = argv[1];
 
   pid_t pid;
 
-  printf("Parent: Playing Chinese whisper game with %d processes\n",childNum);
+  printf("Parent: Playing Chinese whisper game with %d processes\n", childNum);
 
+  // for loop iterated for the number of childs
   for (int i = 0; i < childNum; i++)
   {
+    // wait until there is no other child process is running
     wait(NULL);
-      pid = fork();
-      if (pid == 0 && i == 0)
-      {
-          sprintf(newargv[1], "%d", i);
-          execve(consumerName, newargv, newenviron);
-          kill(getpid(), SIGTERM);
-      }
-      else if (pid == 0 && i > 0 && i + 1 != childNum)
-      {
-        sprintf(newargv[1], "%d",i);
-        execve(consumerName, newargv, newenviron);
-        kill(getpid(), SIGTERM);
-      }
-      else if (pid == 0 && i + 1 == childNum)
-      {
-        sprintf(newargv[1], "%d", i* (-1));
-        execve(consumerName, newargv, newenviron);
-        kill(getpid(), SIGTERM);
-      }
-      else if (pid > 0)
-      {
-        
-      }
+    pid = fork();
+    // case of the first child
+    if (pid == 0 && i == 0)
+    {
+
+      sprintf(newargv[1], "%d", i);
+      execve(consumerName, newargv, newenviron);
+      kill(getpid(), SIGTERM);
+    }
+    // case for the children which are not first and not last
+    else if (pid == 0 && i > 0 && i + 1 != childNum)
+    {
+      // consumer-producer program will be called
+      // with the parameter i(ith child)
+      sprintf(newargv[1], "%d", i);
+      execve(consumerName, newargv, newenviron);
+      kill(getpid(), SIGTERM);
+    }
+    // case for the last child
+    else if (pid == 0 && i + 1 == childNum)
+    {
+      // consumer-producer program will be called
+      // with the parameter -i(last child)
+      sprintf(newargv[1], "%d", i * (-1));
+      execve(consumerName, newargv, newenviron);
+      kill(getpid(), SIGTERM);
+    }
+    else if (pid > 0)
+    {
+    }
   }
-      wait(NULL);
-      kill(getpid(),SIGTERM);
+  // wait until all other processes are completed and terminate the parent process
+  wait(NULL);
+  kill(getpid(), SIGTERM);
 
   return 0;
 }
